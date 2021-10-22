144a07731e09778d297df67157110a35401ba91f
kalachlegends <90833872+kalachlegends@users.noreply.github.com>
2021-10-22 23:51:59 +0600

    Прошелся через f12
    теперь вроде на всех разрешениях смотреться отлично

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 });
 $(document).ready(function() {
 	$('.header__burger320-main').click(function(event) {
-		$('.header__burger320,.header__menu320-main').toggleClass('active');
+		$('.header__burger320-main,.header__menu320-main').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 });
